19757cd588eedcdfbe902d06d24436c9d9901db0
Breno Martins Simão <brenomsimao123@outlook.com>
2023-08-06 23:51:06 -0300

    -> Added database context

---
+++ MoviesAPI/Database/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using MoviesAPI.Models.Database;
+using System.Data.Entity;
+
+namespace MoviesAPI.Database
+{
+#nullable disable
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base()
+        {}
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<SearchHistory> SearchHistories { get; set; }
+    }
+}
---
+++ MoviesAPI/Models/Database/SearchHistory.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesAPI.Models.Database
+{
+#nullable disable
+    public class SearchHistory
+    {
+        public int Id { get; set; }
+        public string Query { get; set; }
+        public Movie MovieResulted { get; set; }
+    }
+}
